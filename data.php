dcbfe026333274a822d40620f3a70a3362cae16b
Antonio Kayky G V. <kitzyoff@gmail.com>
2025-03-21 12:06:16 -0300

    Add files via upload

---
+++ data.php
@@ -0,0 +1,17 @@
+<?php
+
+$sentimentData = [
+    'positive' => 60,
+    'neutral' => 25,
+    'negative' => 15,
+];
+
+$latestAnalyses = [
+    ['text' => 'Este produto é incrível!', 'sentiment' => 'positive'],
+    ['text' => 'Não gostei muito da experiência.', 'sentiment' => 'negative'],
+    ['text' => 'É ok, nada de especial.', 'sentiment' => 'neutral'],
+    ['text' => 'Adorei o atendimento!', 'sentiment' => 'positive'],
+    ['text' => 'Que decepção...', 'sentiment' => 'negative'],
+];
+
+?>
