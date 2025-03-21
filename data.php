833a46532717844f7a5b244998898495bbbf230e
Antonio Kayky G V. <kitzyoff@gmail.com>
2025-03-21 15:47:17 -0300

    Update data.php

--- data.php
+++ data.php
@@ -5,7 +5,7 @@ $sentimentData = [
     'neutral' => 25,
     'negative' => 15,
 ];
-
+ /// Aqui é só um exemplo quem for analisar, pode ficar a vontade pra apagar, mas precisa atualizar a pagina.
 $latestAnalyses = [
     ['text' => 'Este produto é incrível!', 'sentiment' => 'positive'],
     ['text' => 'Não gostei muito da experiência.', 'sentiment' => 'negative'],
